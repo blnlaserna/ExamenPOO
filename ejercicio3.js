f643b26f48b537a6744695f8013ecdda2722b889
Belen LB <blaserna@iessonferrer.net>
2021-12-20 16:40:21 +0100

    Solución examen POO

---
+++ ejercicio3.js
@@ -0,0 +1,13 @@
+const registroPacientes = new Map([
+    [`AAA024`, `Fernández M. (321790059) -> C/Recoletos, 50`],  
+    [`BCD827`, `Ruíz P. (100973253) -> C/Esquerdo izquierdo, 103`],
+    [`YUN835`,`Benítez E. (154811767) -> Av.Argentina, 5`]
+]);
+
+
+var numeroRegistroPaciente;
+var nombrePaciente;
+var numeroSSPaciente;
+var direccionPaciente;
+const agendaPacientes = new Map();
+var contadorPacientes = 0;
